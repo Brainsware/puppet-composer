6f15ac1a464eaff34893e56428d3d93d06d75d03
Igor Galić <i.galic@brainsware.org>
2013-09-16 11:11:00 +0200

    add defined type to install & update a project
    This gives us a defined type for installing and updating composer
    projects. We're also adding test cases here which are actually
    respectable examples ;)

---
+++ manifests/project.pp
@@ -0,0 +1,98 @@
+#   Copyright 2013 Brainsware
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+# = Defined Type composer::project
+#
+#  This defined type helps install a project with composer and keep it up-todate
+#
+# == Parameters:
+#
+# [*ensure*]
+#   Can be either 'present' or 'latest. Default: present
+#
+# [*target*]
+#   Where to install this composer project. Must exist! Defaults to $title
+#
+# [*dev*]
+#   Toggle the installation of require-dev packages. Default: false
+#
+# [*scripts*]
+#   Toggles the execution of all scripts defined in composer.json. Default: true
+#
+# [*custom_inst*]
+#   Toggles the execution of all custom installers. Default: true
+#
+# [*prefer*]
+#   Define a preference for either 'source' or 'dist' (package) distribution. Default: 'dist'
+#
+# [*lock*]
+#   Toggles whether to update only the hash in composer.lock to avoid "out of date" warnings. Default: false
+#
+
+define composer::project (
+  $ensure      = present,
+  $target      = $title,
+  $dev         = false,
+  $prefer      = 'dist',
+  $scripts     = true,
+  $custom_inst = true,
+  $lock        = false,
+) {
+  include composer
+
+  validate_re($ensure, '^(present|latest)$', '$ensure must be one of present or latest.')
+  validate_re($prefer, '^(dist|source)$', '$prefer can only be one of source or dist. See `composer install --help`.')
+  validate_bool($dev, $scripts, $custom_inst)
+
+  $composer  = "${composer::target_dir}/${composer::command_name}"
+  $base_opts = '--no-interaction --quiet --no-progress'
+
+  $dev_opt = $dev? {
+    true  => '--dev',
+    false => '--no-dev',
+  }
+  $script_opt = $scripts? {
+    true  => [],
+    false => '--no-scripts',
+  }
+  $custom_inst_opt = $custom_inst? {
+    true  => [],
+    false => '--no-custom-installer',
+  }
+  $lock_opt = $lock? {
+    false => [],
+    true  => '--lock',
+  }
+
+  $install_opts = join(flatten([$dev_opt, $script_opt, $custom_inst_opt, "--prefer-${prefer}" ]), ' ')
+  $update_opts = join(flatten([$dev_opt, $script_opt, $custom_inst_opt, "--prefer-${prefer}", $lock_opt ]), ' ')
+
+
+  exec { "composer_install_${title}":
+    command => "${composer} install ${base_opts} ${install_opts}",
+    onlyif  => "${composer} install ${install_opts} --dry-run | grep -- '- Installing '",
+    cwd     => $target,
+    require => Class['composer'],
+  }
+
+  if $ensure == latest {
+    exec { "composer_update_${title}":
+      command => "${composer} update ${base_opts} ${update_opts}",
+      onlyif  => "${composer} update ${update_opts} --dry-run | grep -- '- Installing '",
+      cwd     => $target,
+      path    => $::path,
+      require => Class['composer'],
+    }
+  }
+}
---
+++ spec/defines/project_spec.rb
@@ -0,0 +1,75 @@
+#   Copyright 2013 Brainsware
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+require 'spec_helper'
+
+describe 'composer::project' do
+  let(:title) { 'yolo' }
+  let(:params) {{ :target => '/srv/web/yolo' }}
+
+  it { should include_class('composer') }
+  it { should contain_composer__project('yolo').with({ 'ensure' => 'present'} )}
+  it { should contain_exec('composer_install_yolo').with({
+    'command' => '/usr/local/bin/composer install --no-interaction --quiet --no-progress --no-dev --prefer-dist',
+    'cwd'     => '/srv/web/yolo'
+  })}
+
+  describe 'composer project with ensure => latest' do
+    let(:title) { 'yolo' }
+    let(:params) {{ :target => '/srv/web/yolo', :ensure => 'latest' }}
+
+    it { should include_class('composer') }
+    it { should contain_composer__project('yolo').with({ 'ensure' => 'latest'} )}
+    it { should contain_exec('composer_install_yolo').with({
+      'command' => '/usr/local/bin/composer install --no-interaction --quiet --no-progress --no-dev --prefer-dist',
+      'cwd'     => '/srv/web/yolo'
+    })}
+    it { should contain_exec('composer_update_yolo').with({
+      'command' => '/usr/local/bin/composer update --no-interaction --quiet --no-progress --no-dev --prefer-dist',
+      'cwd'     => '/srv/web/yolo'
+    })}
+  end
+
+  describe 'Keep composer project up-to-date with dev dependencies' do
+    let(:title) { 'yolo' }
+    let(:params) {{ :target => '/srv/web/yolo', :ensure => 'latest', :dev => true }}
+
+    it { should include_class('composer') }
+    it { should contain_composer__project('yolo').with({ 'ensure' => 'latest'} )}
+    it { should contain_exec('composer_install_yolo').with({
+      'command' => '/usr/local/bin/composer install --no-interaction --quiet --no-progress --dev --prefer-dist',
+      'cwd'     => '/srv/web/yolo'
+    })}
+    it { should contain_exec('composer_update_yolo').with({
+      'command' => '/usr/local/bin/composer update --no-interaction --quiet --no-progress --dev --prefer-dist',
+      'cwd'     => '/srv/web/yolo'
+    })}
+  end
+
+  describe 'Keep composer project up-to-date with dev dependencies, ignoring composer.lock' do
+    let(:title) { 'yolo' }
+    let(:params) {{ :target => '/srv/web/yolo', :ensure => 'latest', :dev => true, :lock => true }}
+
+    it { should include_class('composer') }
+    it { should contain_composer__project('yolo').with({ 'ensure' => 'latest'} )}
+    it { should contain_exec('composer_install_yolo').with({
+      'command' => '/usr/local/bin/composer install --no-interaction --quiet --no-progress --dev --prefer-dist',
+      'cwd'     => '/srv/web/yolo'
+    })}
+    it { should contain_exec('composer_update_yolo').with({
+      'command' => '/usr/local/bin/composer update --no-interaction --quiet --no-progress --dev --prefer-dist --lock',
+      'cwd'     => '/srv/web/yolo'
+    })}
+  end
+end
